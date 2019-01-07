09d5dfc957d9fa82304b7d16db600edd42680e14
abc <abc@example.com>
2019-01-07 09:02:11 -0800

    set up dependencies. added map render

--- src/App.js
+++ src/App.js
@@ -2,27 +2,42 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+import DeckGL, {LineLayer} from 'deck.gl';
+import {StaticMap} from 'react-map-gl';
+
+// Set your mapbox access token here
+const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiamZyb2xvdiIsImEiOiJjanFta2JhMTAzdGVsNDRsYjZjbnB2aGk2In0.E1v_EBQE7FeLEx_q0S3ELg';
+
+// Initial viewport settings
+const initialViewState = {
+  longitude: -122.41669,
+  latitude: 37.7853,
+  zoom: 13,
+  pitch: 36,
+  bearing: 0
+};
+
+// Data to be used by the LineLayer
+const data = [{sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781]}];
+
+
+class App extends React.Component {
   render() {
+    const layers = [
+      new LineLayer({id: 'line-layer', data})
+    ];
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload. erer
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <DeckGL
+        initialViewState={initialViewState}
+        controller={true}
+        layers={layers}
+      >
+        <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
+      </DeckGL>
     );
   }
 }
+
 
 export default App;
