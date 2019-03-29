379b2c12a9e0b07e5ae6cb01b8fd0055637a80ff
abc <abc@example.com>
2019-03-28 18:50:22 -0700

    hooked up open sky api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-import DeckGL, {LineLayer} from 'deck.gl';
+import DeckGL, {ScatterplotLayer} from 'deck.gl';
+import * as d3 from 'd3';
 import {StaticMap} from 'react-map-gl';
 
 // Set your mapbox access token here
@@ -18,20 +18,70 @@ const initialViewState = {
 };
 
 // Data to be used by the LineLayer
-const data = [{sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781]}];
+
 
 
 class App extends React.Component {
+
+
+  state = {
+    planes: []
+  }
+
+
+  componentDidMount(){
+    this.fetchFlightData()
+  }
+
+  fetchFlightData = () => {
+
+    let app = this
+
+    fetch('https://opensky-network.org/api/states/all')
+    .then(res => res.json())
+    .then(function(myJson) {
+      let data = myJson.states;
+      console.log(typeof data)
+      return app.setState({
+        planes: data.map(d => ({
+          callsign: d[1],
+          long: d[5],
+          lat: d[6],
+          velocity: d[9],
+          alt: d[13],
+          orgin: d[16]
+        }))
+      })
+
+    });
+
+  }
+  
+
   render() {
-    const layers = [
-      new LineLayer({id: 'line-layer', data})
-    ];
+
+    const layer = new ScatterplotLayer({
+      id: 'scatterplot-layer-planes',
+      data: this.state.planes,
+      pickable: false,
+      opacity: 0.3,
+      filled: true,
+      radiusMinPixels: 10,
+      getPosition: d => [d.long, d.lat],
+      getColor: d => [255, 140, 0],
+      // onHover: ({d, x, y}) => {
+      //   const tooltip = `${d.callsign}\n${d.orgin}`;
+
+      // }
+    });
+
+    
 
     return (
       <DeckGL
         initialViewState={initialViewState}
         controller={true}
-        layers={layers}
+        layers={layer}
       >
         <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
       </DeckGL>
