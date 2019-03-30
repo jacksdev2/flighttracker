a2402f5306054ba5d3cd07d3fdf3022248cc7b92
abc <abc@example.com>
2019-03-30 15:02:50 -0700

    added plane icon and updaterfunction to call api e10sec

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import DeckGL, {ScatterplotLayer} from 'deck.gl';
+import DeckGL, {IconLayer} from 'deck.gl';
 import * as d3 from 'd3';
 import {StaticMap} from 'react-map-gl';
 
+import PlaneIcon from './images/plane.png';
 // Set your mapbox access token here
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiamZyb2xvdiIsImEiOiJjanFta2JhMTAzdGVsNDRsYjZjbnB2aGk2In0.E1v_EBQE7FeLEx_q0S3ELg';
 
@@ -12,7 +13,7 @@ const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiamZyb2xvdiIsImEiOiJjanFta2JhMTAzdGVsNDRs
 const initialViewState = {
   longitude: -122.41669,
   latitude: 37.7853,
-  zoom: 13,
+  zoom: 5,
   pitch: 36,
   bearing: 0
 };
@@ -31,7 +32,6 @@ class App extends React.Component {
 
   componentDidMount(){
     this.fetchFlightData()
-    console.log()
   }
 
   fetchFlightData = () => {
@@ -50,26 +50,30 @@ class App extends React.Component {
           lat: d[6],
           velocity: d[9],
           alt: d[13],
-          orgin: d[16]
+          orgin: d[16],
+          true_track: d[10]
         }))
       })
-
     });
-
+    setTimeout(this.fetchFlightData, 10 * 1000)
   }
   
 
   render() {
 
-    const layer = new ScatterplotLayer({
-      id: 'scatterplot-layer-planes',
+    const layer = new IconLayer({
+      id: 'planes',
       data: this.state.planes,
       pickable: false,
+      iconAtlas: PlaneIcon,
+      iconMapping: {
+        marker: {x: 0, y: 0, width: 532, height: 532, mask: false}
+      },
+      getIcon: d => "marker",
+      sizeScale: 35,
       opacity: 0.3,
-      filled: true,
-      radiusMinPixels: 10,
       getPosition: d => [d.long, d.lat],
-      getColor: d => [255, 140, 0],
+      getAngle: d => 65 + (d.true_track * 180) / Math.PI,
       // onHover: ({d, x, y}) => {
       //   const tooltip = `${d.callsign}\n${d.orgin}`;
 
